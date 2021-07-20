d9ce3ef27e121d8a54c027d2d6c475a60ed606c5
LuyandaDingindlela101 <3995219@myuwc.ac.za>
2021-07-20 18:40:16 +0200

    added template and static folders to display the info better

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 import requests
 
 
@@ -7,8 +7,7 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def get_chuck_norris_jokes():
-
     api_url = "https://api.chucknorris.io/jokes/random"
-    response = requests.get(api_url).json()
+    joke_details = requests.get(api_url).json()
 
-    return "<strong>Random joke from chuck norris: </strong>" + response['value']
+    return render_template("view_joke.html", joke_details=joke_details)
